10dc1d427b9efec30eac7d7b2ce3b85bc87cfc77
Suman Kumar <sumanqaj9876@gmail.com>
2025-11-23 18:45:23 +0530

    Fix import path for wishlistReducer to match case sensitivity

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import myReducer from "./cartSlice";
-import wishlistReducer from "./wishlistSlice";
+import wishlistReducer from "./wishListSlice";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // ✅ Local Storage
 
